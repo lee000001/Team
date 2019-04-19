3d1da6ce79ee84a7febe560679e1fec01e09cf03
lee <1047662425@qq.com>
2019-04-19 15:45:21 +0800

    服务的使用以及使用服务进行周期性任务

--- Team/demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
+++ Team/demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -11,6 +11,6 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 62;
   public static final String VERSION_NAME = "6.1.2";
   // Fields from default config.
-  public static final String BUILD_DATE = "2019-4-19 13:20:28";
-  public static final String GIT_REVISION = "07bf82c";
+  public static final String BUILD_DATE = "2019-4-19 14:38:16";
+  public static final String GIT_REVISION = "14789ce";
 }
