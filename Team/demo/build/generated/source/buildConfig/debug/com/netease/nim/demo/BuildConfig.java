d47a031da6cf64224dd36ca19808b059f437660c
lee <1047662425@qq.com>
2019-04-24 20:53:30 +0800

    2019年4月24日20:53:23

--- Team/demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
+++ Team/demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -11,6 +11,6 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 62;
   public static final String VERSION_NAME = "6.1.2";
   // Fields from default config.
-  public static final String BUILD_DATE = "2019-4-20 15:34:47";
-  public static final String GIT_REVISION = "2da9250";
+  public static final String BUILD_DATE = "2019-4-24 20:50:16";
+  public static final String GIT_REVISION = "3058dbf";
 }
