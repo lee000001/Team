19a6a988111ceeb1d01f23327e344af1a25f7fa6
lee <1047662425@qq.com>
2019-05-10 19:46:01 +0800

    2019年5月10日19:45:58

---
+++ checklistview/app/src/main/java/com/example/testchecklistview/DataHolder.java
@@ -0,0 +1,24 @@
+package com.example.testchecklistview;
+
+
+public class DataHolder{
+
+    public String titleStr;
+
+    public String subTitleStr;
+
+    public boolean checked;
+
+
+
+    public DataHolder(String title,String subTitle,boolean check){
+
+        titleStr = title;
+
+        subTitleStr = subTitle;
+
+        checked=check;
+
+    }
+
+}
---
+++ checklistview/app/src/main/java/com/example/testchecklistview/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.testchecklistview;
+
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.util.Log;
+import android.view.View;
+
+import android.widget.AdapterView;
+
+import android.widget.Button;
+
+import android.widget.ListView;
+
+
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    private static final String TAG = "MainActivity";
+
+    private ListView listView;
+
+    private Button all_sel;
+
+    private Button all_unsel;
+
+    private ListitemAdapter  adapter;
+
+    private List<DataHolder> datalist;
+    private Button send;
+    private List<DataHolder> selectedDatas;
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        listView= (ListView) this.findViewById(R.id.list);
+
+        all_sel= (Button) this.findViewById(R.id.all_sel);
+
+        all_unsel= (Button) this.findViewById(R.id.all_unsel);
+        send = findViewById(R.id.send);
+        send.setOnClickListener(this);
+
+        all_sel.setOnClickListener(this);
+
+        all_unsel.setOnClickListener(this);
+
+
+
+        //初始化数据
+
+        datalist=new ArrayList<DataHolder>();
+
+        for (int i = 0; i <10 ; i++) {
+
+            datalist.add(new DataHolder("今日头条:"+i,"内容:"+i,false));
+
+        }
+
+
+
+        //创建数据适配器并且绑定数据
+
+        adapter=new ListitemAdapter(MainActivity.this,datalist);
+
+        listView.setAdapter(adapter);
+
+
+
+        //设置列表点击事件
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //记录选中item
+
+                boolean checked = datalist.get(position).checked;
+
+                if (!checked) {
+
+                    datalist.get(position).checked = true;
+
+                }else {
+
+                    datalist.get(position).checked = false;
+
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+        });
+
+    }
+
+
+
+    @Override
+
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            //设置全部选中
+
+            case R.id.all_sel:
+
+                for (int i = 0; i <datalist.size() ; i++) {
+
+                    datalist.get(i).checked=true;
+
+                }
+
+                adapter.notifyDataSetChanged();
+
+                break;
+
+            //取消全部选中
+
+            case R.id.all_unsel:
+
+                for (int i = 0; i <datalist.size() ; i++) {
+
+                    datalist.get(i).checked=false;
+
+                }
+
+                adapter.notifyDataSetChanged();
+
+                break;
+            case R.id.send:
+                selectedDatas = new ArrayList<>();
+                for(DataHolder d:datalist){
+                    if(d.checked){
+                        selectedDatas.add(d);
+                    }
+                }
+                Log.d(TAG, "onClick: "+selectedDatas.toString());
+                break;
+
+        }
+
+    }
+
+}
